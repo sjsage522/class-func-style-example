8bf495dd55eaf0c6c7b0baa62bf0d4d2d7f1faa2
shinjunseok <sjsage522@gmail.com>
2021-09-15 01:49:12 +0900

    update : App.js
    - 클래스 형식 컴포넌트의 라이프 사이클 구현

--- src/App.js
+++ src/App.js
@@ -39,13 +39,43 @@ function FuncComp(props) {
     )
 }
 
+const classStyle = 'color:green';
 class ClassComp extends React.Component {
     state = {
         number: this.props.initNumber,
         date: (new Date()).toString(),
     }
 
+    //render 호출 전
+    static getDerivedStateFromProps() {
+        console.log('%cclass => getDerivedStateFromProps', classStyle);
+        return {};
+    }
+
+    //컴포넌트가 생성되고 render 호출 후, 최초 한번
+    componentDidMount() {
+        console.log('%cclass => componentDidMount', classStyle);
+    }
+
+    //render 를 호출할 필요가 있는지? true -> render 호출o, false -> 호출x
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log('%cclass => shouldComponentUpdate', classStyle);
+        return true;
+    }
+
+    //컴포넌트 업데이트 시 render 호출 후
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('%cclass => getSnapshotBeforeUpdate', classStyle);
+        return null;
+    }
+
+    //컴포넌트 업데이트 시 getSnapshotBeforeUpdate 호출 후
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('%cclass => componentDidUpdate', classStyle);
+    }
+
     render() {
+        console.log('%cclass => render', classStyle);
         return (
             <div className="container">
                 <h2>class style component</h2>

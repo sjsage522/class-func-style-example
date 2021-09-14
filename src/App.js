199fc0ae18cfbc29c2ee25b5b13310cef3724d9c
shinjunseok <sjsage522@gmail.com>
2021-09-15 01:02:36 +0900

    update : App.js
    - 실습 (상태변경을 통해 날짜 표시 하기)

--- src/App.js
+++ src/App.js
@@ -15,15 +15,24 @@ function FuncComp(props) {
     const numberState = useState(props.initNumber);
     let number = numberState[0]; /* state */
     let setNumber = numberState[1]; /* function that changes state */
+
+    const [date, setDate] = useState(new Date().toString()); /* destructuring assignment */
+
     console.log('numberState', numberState); /* Array(2) */
 
     return (
         <div className="container">
             <h2>function style component</h2>
             <p>Number : {number}</p>
+            <p>Date : {date}</p>
             <input type="button" value="random" onClick={
                 () => {
                     setNumber(Math.random())
+                }
+            }/>
+            <input type="button" value="date" onClick={
+                () => {
+                    setDate(new Date().toString())
                 }
             }/>
         </div>
@@ -33,6 +42,7 @@ function FuncComp(props) {
 class ClassComp extends React.Component {
     state = {
         number: this.props.initNumber,
+        date: (new Date()).toString(),
     }
 
     render() {
@@ -40,9 +50,15 @@ class ClassComp extends React.Component {
             <div className="container">
                 <h2>class style component</h2>
                 <p>Number : {this.state.number}</p>
+                <p>Date : {this.state.date}</p>
                 <input type="button" value="random" onClick={
                     () => {
-                        this.setState({number:Math.random()})
+                        this.setState({number: Math.random()})
+                    }
+                }/>
+                <input type="button" value="date" onClick={
+                    () => {
+                        this.setState({date: (new Date().toString())})
                     }
                 }/>
             </div>
